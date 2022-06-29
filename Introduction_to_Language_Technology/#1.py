4200986e5463153c8e17d287adacbd65d7ed7d1f
Sangwon Roh <sangwenyroh@gmail.com>
2022-06-29 17:49:52 +0900

    new

--- Introduction_to_Language_Technology/#1.py
+++ Introduction_to_Language_Technology/#1.py
@@ -1,33 +1,36 @@
+import os
+
 def fileopen(f):
     file = open(f,'r')
+    global data
     data = file.read()
-    data_list = data.replace("=","").split()
-    file.close
-    return(data_list)
 
 def word_duration (file_list, sen):
     count = 1
     for filename in file_list:
-        data_list = fileopen(filename)
+        
+        fileopen(filename)
+        data_list = data.split()
         total = 0
-        print('#', count)
+        print('#', count) 
         
         for word in sen.split():
             for d in data_list:
                 if d == '"'+word+'"':
                     i = data_list.index(d)
-                    dur = float(data_list[i-2])-float(data_list[i-4])
+                    dur = float(data_list[i-3])-float(data_list[i-5])
                     total = total + dur
                     print("duration of words", d, round(dur,3))
         
         print("total = " , round(total,3))
         count = count + 1
 
-import os
-path = "./"
-file_list = os.listdir(path) #현재 경로에서의 file list를 file_list에 저장
-file_list_textgrid = [file for file in file_list if file.endswith(".TextGrid")]
-file_list = file_list_textgrid
+def file_lister(p):
+    file_list = os.listdir(p) #현재 경로에서의 file list를 file_list에 저장
+    file_list_textgrid = [file for file in file_list if file.endswith(".TextGrid")]
+    file_list = file_list_textgrid
+    return file_list
 
 sen = "push the button"
+file_list = file_lister("C:/Users/sangw/Desktop/DataCampus/Introduction_to_Language_Technology")
 word_duration(file_list, sen)
